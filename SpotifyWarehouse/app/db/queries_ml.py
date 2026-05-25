5b1b361f7efb5393ad754d12fa7539ec7927a590
DemiFiend01 <maggie.mr1606@gmail.com>
2026-05-25 10:29:57 +0200

    Setup for mimic mdx

---
+++ SpotifyWarehouse/app/db/queries_ml.py
@@ -0,0 +1,6 @@
+import psycopg
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Create functions with sql queries and then plotlib plot.
+# manually or not move the output to flask outputs
